d742af40da55d18c938ed3a9f3664737b366d75a
RajrupDasid <rajrupdasid@outlook.com>
2021-07-24 20:10:31 +0530

    tweaked for heroku

--- src/music_controller/settings.py
+++ src/music_controller/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'music_controller.urls'
@@ -135,8 +139,10 @@ STATIC_ROOT=os.path.join(BASE_DIR,'static_cdn')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media_cdn')
 
 TEMP=os.path.join(BASE_DIR,'media_cdn/temp')
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
