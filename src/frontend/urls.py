e415a04a5df809a47a9e7aeb41a5484f89fb865a
RajrupDasid <rajrupdasid@outlook.com>
2021-07-24 19:54:50 +0530

    untracked files

---
+++ src/frontend/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import index
+app_name='frontend'
+urlpatterns = [
+    path('', index),
+    path('info',index),
+    path('join', index),
+    path('create', index),
+    path('room/<str:roomCode>', index)
+]
+
